aa0e4e8fa1410586f7af63f9c57128a09b563af1
lakvindu <jal.siriwardena@gmail.com>
2026-09-01 19:30:38 +0530

    feat: make WHT rates effective-date driven, guard base tax setup from resurrection

--- erpnext_srilankan_localisation/overrides/company.py
+++ erpnext_srilankan_localisation/overrides/company.py
@@ -1,8 +1,16 @@
-from erpnext_srilankan_localisation.setup.tax_templates import create_sri_lanka_tax_setup
+import frappe
+
+from erpnext_srilankan_localisation.setup.tax_templates import create_sri_lanka_tax_setup, sync_sscl_sales_templates
+from erpnext_srilankan_localisation.setup.wht_categories import create_wht_categories
 
 
 def on_update(doc, method=None):
 	if doc.country != "Sri Lanka":
 		return
 
-	create_sri_lanka_tax_setup(doc.name)
+	if not doc.get("lk_tax_setup_completed"):
+		if create_sri_lanka_tax_setup(doc.name):
+			create_wht_categories()
+			frappe.db.set_value("Company", doc.name, "lk_tax_setup_completed", 1)
+
+	sync_sscl_sales_templates(doc.name)
